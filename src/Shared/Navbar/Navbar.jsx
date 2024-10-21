d5877f51ef24cb911de19eded712174500b760c5
Israt85 <isratanjuman3@gmail.com>
2024-10-21 14:01:51 +0600

    fixed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from '../../assets/Logotraining.png'
-import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/Logotraining.png';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
- 
-    return (
-      <div className="navbar bg-base-100">
+  return (
+    <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className=" lg:hidden">
+          <div tabIndex={0} role="button" className="lg:hidden">
             <svg 
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -24,45 +23,38 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-neutral rounded-box z-[1] mt-3 w-52 p-2 shadow">
-             <NavLink to='/'  className={({ isActive }) =>
-        isActive
-          ? 'text-[#D0FD3E] font-bold'
-          : 'text-gray-100'  
-      }> <li><a>Home</a></li></NavLink>
-     <NavLink to='/about'  className={({ isActive }) =>
-        isActive
-          ? 'text-[#D0FD3E] font-bold'
-          : 'text-gray-100'  
-      }> <li><a>About</a></li></NavLink>
-  <NavLink to='/contact'>  <li className='w-[100px] h-[40px] text-black bg-[#D0FD3E] flex justify-center items-center rounded-full'>
-      <a className='px-6'>Contact</a>
-    </li></NavLink>
+            className="menu menu-sm dropdown-content bg-neutral rounded-box z-[1] mt-3 w-52 p-2">
+            <NavLink to='/' className={({ isActive }) => isActive ? 'text-[#D0FD3E] font-bold' : 'text-gray-100'}>
+              <li><a className="focus:outline-none">Home</a></li>
+            </NavLink>
+            <NavLink to='/about' className={({ isActive }) => isActive ? 'text-[#D0FD3E] font-bold' : 'text-gray-100'}>
+              <li><a className="focus:outline-none">About</a></li>
+            </NavLink>
+            <li className='w-[100px] h-[40px] text-black bg-[#D0FD3E] flex justify-center items-center rounded-full focus:outline-none'>
+              <a href='/contact' className='px-6 focus:outline-none'>Contact</a>
+            </li>
           </ul>
         </div>
         <div className="">
-  <img className=' w-[80px] h-[80] mx-2 lg:ml-20' src={logo} alt="" />
-</div>
+          <img className='w-[80px] h-[80px] mx-2 lg:ml-20' src={logo} alt="" />
+        </div>
       </div>
       <div className="navbar-end hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-        <NavLink to='/'  className={({ isActive }) =>
-        isActive
-          ? 'text-[#D0FD3E] font-bold'
-          : 'text-gray-100'  
-      }> <li><a>Home</a></li></NavLink>
-     <NavLink to='/about'  className={({ isActive }) =>
-        isActive
-          ? 'text-[#D0FD3E] font-bold'
-          : 'text-gray-100'  
-      }> <li><a>About</a></li></NavLink>
-  <Link to='/contact'>  <li className='w-[100px] h-[40px] text-black bg-[#D0FD3E] rounded-full'>
-      <a className='px-6'>Contact</a>
-    </li></Link>
+          <NavLink to='/' className={({ isActive }) => isActive ? 'text-[#D0FD3E] font-bold' : 'text-gray-100'}>
+            <li><a className="focus:outline-none">Home</a></li>
+          </NavLink>
+          <NavLink to='/about' className={({ isActive }) => isActive ? 'text-[#D0FD3E] font-bold' : 'text-gray-100'}>
+            <li><a className="focus:outline-none">About</a></li>
+          </NavLink>
+            <li className='w-[100px] h-[40px] text-black bg-[#D0FD3E] flex justify-center items-center rounded-full focus:outline-none'>
+              <a href='/contact' className='px-6 focus:outline-none'>Contact</a>
+            </li>
+        
         </ul>
       </div>
     </div>
-    );
+  );
 };
 
 export default Navbar;
